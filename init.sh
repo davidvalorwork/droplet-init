7c90d05dd874ef0e8cba0a45c3e1e35e4fa6b05f
David Valor <david.valor@geoselfie.com.au>
2022-03-02 11:52:44 -0400

    adding

--- init.sh
+++ init.sh
@@ -6,3 +6,9 @@ git clone git@github.com:davidvalorwork/notas.git
 
 echo [Init] Cloning all projects
 bash ~/notas/scripts/repos
+
+echo [Init] Installing packages
+bash ~/notas/scripts/install-packages
+
+echo [Init] Creating SSL
+bash ~/projects/droplet-init/ssl-init.sh
---
+++ ssl-init.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+echo [SSL_INIT] Copying SSL files
+rm -rf /etc/apache2
+cp ~/projects/ssl-david/apache2 /etc/apache2
+rm -rf /etc/ssl
+cp ~/projects/ssl-david/apache2 /etc/ssl
+
+echo [SSL_INIT] Enablingn a2enmod ssl
+sudo a2enmod ssl
+
+echo [SSL_INIT] Enablingn a2enmod rewrite
+sudo a2enmod rewrite
+
+echo [SSL_INIT] Restarting apache2
+sudo service apache2 restart
+
